6365910067fffcf5ea6b376e2566a688ec2a7af2
Shilpa Mukhopadhyay <117051201+shilpa2301@users.noreply.github.com>
2023-10-19 19:07:28 -0500

    Add files via upload

--- HUman_Detection/human_detection.py
+++ HUman_Detection/human_detection.py
@@ -6,13 +6,38 @@ import math
 from std_msgs.msg import Float32
 from example_interfaces.msg import Int64
 
-from project3.lidar_custom import *
-from project3.pub_loc_count import *
-from project3.scan_subscriber import *
 
-class Human_Detection( LaserScanToPointCloudNode, PublishPersonLocationCount, LaserScanSubscriber):
-    def __init__(self):
-        super().__init__('intruder_detection')
+class Human_Detection( Node):
+    def __init__(self,node_name):
+        super().__init__(node_name)
+        self.laser_scan_subscriber = self.create_subscription(LaserScan, 'scan', self.laser_scan_callback, 10)
+        self.point_cloud_publisher = self.create_publisher(PointCloud, 'point_cloud', 10)
+
+    def laser_scan_callback(self, scan):
+        point_cloud_msg = self.laser_scan_to_point_cloud(scan)
+        self.point_cloud_publisher.publish(point_cloud_msg)
+        print("published_data=", point_cloud_msg)
+
+    def laser_scan_to_point_cloud(self, scan):
+        point_cloud_data = []
+
+        for i in range(len(scan.ranges)):
+            angle = scan.angle_min + i * scan.angle_increment
+            x = scan.ranges[i] * math.cos(angle)
+            y = scan.ranges[i] * math.sin(angle)
+            z = 0.0
+
+            point32 = Point32()
+            point32.x = x
+            point32.y = y
+            point32.z = z
+            point_cloud_data.append(point32)
+
+        point_cloud_msg = PointCloud()
+        point_cloud_msg.header = scan.header
+        point_cloud_msg.points = point_cloud_data
+
+        return point_cloud_msg
 
     #def get_data(self):
         
@@ -20,9 +45,9 @@ class Human_Detection( LaserScanToPointCloudNode, PublishPersonLocationCount, La
 
 def main(args=None):
     rclpy.init(args=args)
-    node = Human_Detection()
-    rclpy.spin(node)
-    node.destroy_node()
+    node1 = Human_Detection('Node1')
+    rclpy.spin(node1)
+    node1.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
