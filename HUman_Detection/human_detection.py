1b8e4ccca8eb960c99765e96e08cc5a36b89a380
Shilpa Mukhopadhyay <117051201+shilpa2301@users.noreply.github.com>
2023-10-27 13:56:22 -0500

    data storing and kdTRee

--- HUman_Detection/human_detection.py
+++ HUman_Detection/human_detection.py
@@ -5,6 +5,9 @@ from geometry_msgs.msg import Point32
 import math
 from std_msgs.msg import Float32
 from example_interfaces.msg import Int64
+import numpy as np
+from sklearn.neighbors import KDTree
+import time
 
 
 class Human_Detection( Node):
@@ -22,10 +25,13 @@ class Human_Detection( Node):
         self.person_location_publisher = self.create_publisher(PointCloud, '/person_locations', 10)
         self.person_count_publisher = self.create_publisher(Int64, '/person_count', 10)
 
+   
     def laser_scan_callback(self, scan):
         point_cloud_msg = self.laser_scan_to_point_cloud(scan)
         self.point_cloud_publisher.publish(point_cloud_msg)
-        print("published_data=", point_cloud_msg)
+        #print("published_data=", point_cloud_msg.points)
+
+        self.process_data(point_cloud_msg)
     
     def location_callback(self, location):
         self.get_logger().info('I heard: "%s"' % location.data)
@@ -55,8 +61,24 @@ class Human_Detection( Node):
 
         return point_cloud_msg
 
-    #def get_data(self):
-        
+    def process_data(self, point_cloud_msg):
+        data=[]
+        print("before removing=",len(point_cloud_msg.points))
+        for i in range(len(point_cloud_msg.points)):
+            #print("i-th data= ",point_cloud_msg.points[i].x,',',point_cloud_msg.points[i].y)
+            if ((point_cloud_msg.points[i].x!=float('inf') and point_cloud_msg.points[i].x!=float('-inf') and np.isnan(point_cloud_msg.points[i].x)==False) \
+            and (point_cloud_msg.points[i].y!=float('inf') and point_cloud_msg.points[i].y!=float('-inf') and np.isnan(point_cloud_msg.points[i].y)==False) \
+            and point_cloud_msg.points[i].z!=float('inf') and point_cloud_msg.points[i].z!=float('-inf') and np.isnan(point_cloud_msg.points[i].z)==False) :
+            
+                # print([point_cloud_msg.points[i].x, point_cloud_msg.points[i].y, point_cloud_msg.points[i].z])
+                data.append([point_cloud_msg.points[i].x, point_cloud_msg.points[i].y, point_cloud_msg.points[i].z])
+        #print("data=",data)
+        print("length of data=", len(data))
+        time.sleep(2)
+        tree = KDTree(data)
+        num_neighbors = tree.query_radius(data, r = 1.0)
+        print("num_neighbours= ", num_neighbors)
+     
 
 
 def main(args=None):
