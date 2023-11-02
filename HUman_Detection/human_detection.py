f75c4fd318539122faa053355d06b299de1564f3
Shilpa Mukhopadhyay <117051201+shilpa2301@users.noreply.github.com>
2023-11-02 13:58:13 -0500

    Add files via upload

--- HUman_Detection/human_detection.py
+++ HUman_Detection/human_detection.py
@@ -12,10 +12,10 @@ import numpy.linalg as LA
 import time
 
 THRESHOLD_SENSOR_MATCH = 0.2
-THRESHOLD_CENTROIDS =  0.3
-THRESHOLD_RADIUS_KDTREE = 0.5
-THRESHOLD_IOU = 0.95
-THRESHOLD_INTERSECTION = 0.90
+THRESHOLD_CENTROIDS =  1.2
+THRESHOLD_RADIUS_KDTREE = 0.8
+THRESHOLD_IOU = 0.80
+THRESHOLD_INTERSECTION = 0.02
 #FRAME =0
 
 class LaserScanNode(Node):
@@ -25,13 +25,15 @@ class LaserScanNode(Node):
         #self.frame = 0
         self.first_frame_data=[]
         self.common_data =[]
+        self.curr_frame_data=[]
 
 
         self.frame_id =0
+        print("init called")
 
         #create subscriber to /frame id topic
-        self.frame_subscriber = self.create_subscription(Int64, 'frame_id', self.frame_callback, 10)
-        self.frame_publisher = self.create_publisher(Int64, 'frame_id', 10)
+        #self.frame_subscriber = self.create_subscription(Int64, 'frame_id', self.frame_callback, 10)
+        #self.frame_publisher = self.create_publisher(Int64, 'frame_id', 10)
         
 
         self.laser_scan_subscriber = self.create_subscription(LaserScan, 'scan', self.laser_scan_callback, 10)
@@ -46,7 +48,7 @@ class LaserScanNode(Node):
         self.frame_id = frame_id
         return frame_id
 
-    def bb_Intersection(boxA, boxB):
+    def bb_Intersection(self,boxA, boxB):
         xA = max(boxA[0], boxB[0])
         yA = max(boxA[1], boxB[1])
         xB = min(boxA[2], boxB[2])
@@ -56,7 +58,7 @@ class LaserScanNode(Node):
 
         return interArea
 
-    def bb_IOU(boxA, boxB):
+    def bb_IOU(self,boxA, boxB):
         # determine the (x, y)-coordinates of the intersection rectangle
         xA = max(boxA[0], boxB[0])
         yA = max(boxA[1], boxB[1])
@@ -87,32 +89,56 @@ class LaserScanNode(Node):
         bb_list =[]
         centr_list =[]
 
-        for i in range(len(ref_frame_data[0])):
-            for j in range(len(curr_frame_data[0])):
-                iou = self.bb_IOU(ref_frame_data[1][i], curr_frame_data[1][j])
+        print("finding common static points")
+        print("ref_frame_data 0 size=", len(ref_frame_data[0]))
+        print("curr_frame_data 0 size=", len(curr_frame_data[0]))
+        print("ref_frame_data 1 size=", len(ref_frame_data[1]))
+        print("curr_frame_data 1 size=", len(curr_frame_data[1]))
+        print("curr_frame_data 1 last data=", curr_frame_data[1][len(curr_frame_data[1])-1])
 
+
+
+        for i in range(len(ref_frame_data[0])):
+            #delete_list=[]
+            for j in range(len(curr_frame_data[0])):
+                print("len of ref, curr=",len(ref_frame_data[0]) ,",",len(curr_frame_data[0]))
+                print()
+                print(i, ",", j)
+                
+
+                iou = self.bb_IOU(ref_frame_data[1][i], curr_frame_data[1][j])
+                print("iou=", iou)
+                
+                
                 if iou > THRESHOLD_IOU:
                     x_low =  min(ref_frame_data[1][i][0],curr_frame_data[1][j][0])
                     x_high = max(ref_frame_data[1][i][2],curr_frame_data[1][j][2])
                     y_low =  min(ref_frame_data[1][i][1],curr_frame_data[1][j][1])
                     y_high = max(ref_frame_data[1][i][3],curr_frame_data[1][j][3])
 
-                    centroid_mean = ((ref_frame_data[0][i]+curr_frame_data[0][j])/2.0)
-
+                    centroid_mean = [(x + y) / 2 for x, y in zip(ref_frame_data[0][i], curr_frame_data[0][j])]
                     bb_list.append([x_low, y_low, x_high, y_high])
                     centr_list.append(centroid_mean)
-                    del ref_frame_data[1][i]
-                    del curr_frame_data[1][j]
-                    del ref_frame_data[0][i]
-                    del curr_frame_data[0][j]
+                    #delete_list.append([i,j])
+
+            
+            #for l in delete_list:
+            #        i = l[0]
+            #        j = l[1]
+#
+            #        del ref_frame_data[1][i]
+            #        del curr_frame_data[1][j]
+            #        del ref_frame_data[0][i]
+            #        del curr_frame_data[0][j]
                 
 
-        common_data.append([centr_list, bb_list])        
+        common_data=[centr_list, bb_list]       
         return common_data
 
     def laser_scan_to_intermediate(self, scan):
         #global FRAME
         #FRAME += 1
+        self.frame_id +=1
         point_cloud_data = []
         point_original=[]
         for i in range(len(scan.ranges)):
@@ -144,15 +170,29 @@ class LaserScanNode(Node):
         bb_list =[]
         centr_list =[]
 
-        for i in range(len(common_data[0])):
-            for j in range(len(data[0])):
-                intersection = self.bb_Intersection(common_data[1][i], data[1][j])
+        #print("common_data in dynamic=", common_data)
+        #time.sleep(2)
+        
+        for j in range(len(data[0])):
+            for i in range(len(common_data[0])):
+            
+                #print()
+                #print(i,",",j)
+                intersection = self.bb_IOU(common_data[1][i], data[1][j])
 
-                if intersection > THRESHOLD_INTERSECTION:
-                    del data[0][j]
-                    del data[1][j]
+                #print("intersection for i,j=", intersection)
+                if intersection < THRESHOLD_INTERSECTION:
+                    #print()
+                    #del data[0][j]
+                    #del data[1][j]
+                    if data[0][j] not in centr_list:
+                        #print("appending=", data[1][j])
+                        bb_list.append(data[1][j])
+                        centr_list.append(data[0][j])
+
+        print("len of dynamic calculated data=", len(bb_list))
                                         
-        dynamic_data = data       
+        dynamic_data = [centr_list, bb_list]       
         
         return dynamic_data
 
@@ -202,7 +242,7 @@ class LaserScanNode(Node):
                     max_x = data[neighbors[0][i]][0]
                 if min_y > data[neighbors[0][i]][1]:
                     min_y = data[neighbors[0][i]][1]
-                if max_y > data[neighbors[0][i]][1]:
+                if max_y < data[neighbors[0][i]][1]:
                     max_y = data[neighbors[0][i]][1]
             value_list=[x/len(neighbors[0]), y/len(neighbors[0]), z/len(neighbors[0])]
             centroids.append(value_list)
@@ -230,7 +270,7 @@ class LaserScanNode(Node):
                     max_x = data[neighbors[n][i]][0]
                 if min_y > data[neighbors[n][i]][1]:
                     min_y = data[neighbors[n][i]][1]
-                if max_y > data[neighbors[n][i]][1]:
+                if max_y < data[neighbors[n][i]][1]:
                     max_y = data[neighbors[n][i]][1]
             centre = [x/len(neighbors[n]), y/len(neighbors[n]), z/len(neighbors[n])]
             #centroids.append([x/len(neighbors[n]), y/len(neighbors[n]), z/len(neighbors[n])])
@@ -250,7 +290,7 @@ class LaserScanNode(Node):
 
             else:    
                 centroids.append(centre)
-                bounding_box.append([min_x, min_y, max_x, max_y])
+                bounding_box.append([min_x-5, min_y-5, max_x+5, max_y+5])
 
                 
         #static vs dynamic
@@ -272,28 +312,51 @@ class LaserScanNode(Node):
         #print("centroids=", centroids)
 
         #/subscribe to /frameid topic = FRAME
+        #print("centroids=", centroids)
         
-        self.curr_frame_data = [centroids, bounding_box]
+        self.curr_frame_data=[centroids, bounding_box]
+        
 
+        print()
         print("framed id=", self.frame_id)
-        
+        print("num of objects before_detection=", len(centroids))
+
+        #print("curr_frame_data=", self.curr_frame_data)
+        #print("first_frame_data=", self.first_frame_data)
+
         if self.frame_id ==1:
+            print("storing frame 1 data")
             self.first_frame_data = self.curr_frame_data
-            # print("first_frame_data=",  self.first_frame_data)
-        elif self.frame_id ==2 and self.frame_id==3:
-            #compare and find common points
-            if self.frame_id ==2:
-                self.common_data = self.first_frame_data
-            print("second/third_frame_data=",  self.common_data)
-            [self.common_data] = self.find_static_common_points(self.common_data, curr_frame_data)
-        elif self.frame_id>=4:
-            data = self.dynamic_point_data(self.common_data, curr_frame_data)
+            data = self.first_frame_data
+            #print("data=",  data)
+        
+        else :
+            data = self.dynamic_point_data(self.first_frame_data, self.curr_frame_data)
+        
+        print("num of objects first_frame=", len(self.first_frame_data[0]))
+        # if self.frame_id ==1:
+        #     print("storing frame 1 data")
+        #     self.first_frame_data = self.curr_frame_data
+        #     data = self.first_frame_data
+        #     #print("data=",  data)
+        # elif self.frame_id ==2 or self.frame_id==3:
+        #     #compare and find common points
+        #     if self.frame_id ==2:
+        #         self.common_data = self.first_frame_data
+        #     #print("second/third_frame_data=",  self.common_data)
+        #     self.common_data = self.find_static_common_points(self.common_data, self.curr_frame_data)
+        #     data = self.common_data
+        # elif self.frame_id>=4:
+        #     data = self.dynamic_point_data(self.common_data, self.curr_frame_data)
 
-        self.frame_publisher.publish(self.frame_id +1)
+        #self.frame_publisher.publish(self.frame_id +1)
 
+        #print ("data before output=", data)
 
+        
         centroids = data[0]
-        print("centroids=", centroids)
+        print("Num of dynamic objects=", len(centroids))
+        #print("centroids=", centroids)
 
         #increment FRAME and publish to /frameid topic
 
@@ -308,7 +371,7 @@ class LaserScanNode(Node):
             centroids_Point.y=cen[1]
             centroids_Point.z=cen[2] 
             data_cloud.append(centroids_Point)   
-        centroids.clear()
+        #centroids.clear()
         #print("num_neighbours= ", num_neighbors)
         point_cloud_msg_ = PointCloud()
         point_cloud_msg_.header = point_cloud_msg.header
@@ -370,12 +433,12 @@ def main(args=None):
     rclpy.init(args=args)
 
     #create Onetime node N1
-    initframe_node = InitFrameNode()
-    initframe_node.publish_frameid()
+    #initframe_node = InitFrameNode()
+    #initframe_node.publish_frameid()
     #destroynode
     print('node_1  start')
     node = LaserScanNode()
-    initframe_node.destroy_node()
+    #initframe_node.destroy_node()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
