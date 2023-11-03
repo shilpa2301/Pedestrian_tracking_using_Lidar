4b457dfb7915018205b9a02023c83b0e5163425a
Shilpa Mukhopadhyay <117051201+shilpa2301@users.noreply.github.com>
2023-11-03 14:20:34 -0500

    Add files via upload

--- HUman_Detection/human_detection.py
+++ HUman_Detection/human_detection.py
@@ -11,12 +11,17 @@ from sklearn.neighbors import KDTree
 import numpy.linalg as LA
 import time
 
+from filterpy.kalman import KalmanFilter
+import math
+from filterpy.common import Q_discrete_white_noise
+
 THRESHOLD_SENSOR_MATCH = 0.2
 THRESHOLD_CENTROIDS =  1.2
 THRESHOLD_RADIUS_KDTREE = 0.8
 THRESHOLD_IOU = 0.80
 THRESHOLD_INTERSECTION = 0.50
 THRESHOLD_CENTROID_DISTANCE_WITH_REF = 2.7
+HUMAN_DIRECTION_THRESHOLD = 30.0
 #FRAME =0
 
 class LaserScanNode(Node):
@@ -41,6 +46,29 @@ class LaserScanNode(Node):
         self.intermediate_publisher = self.create_publisher(PointCloud, 'intermediate_point_cloud', 10)
         self.centroid_publisher = self.create_publisher(PointCloud, 'centroid', 10)
 
+
+        self.human_direction_publisher = self.create_publisher(PointCloud, 'human_direction', 10)
+
+        # Kalman filter initialization
+        #self.kf = KalmanFilter(dim_x=2, dim_z=1)
+        #self.kf.F = np.array([[1., 1.], [0., 1.]])  # State transition matrix
+        #self.kf.H = np.array([[1., 0.]])  # Measurement function
+        #self.kf.P *= 100.0  # Initial state covariance matrix
+        #self.kf.R = 10.0  # Measurement noise
+        #self.kf.Q = Q_discrete_white_noise(dim=2, dt=0.1, var=0.1)  # Process noise
+        #self.human_direction = 0.0
+
+        self.kf = KalmanFilter(dim_x=2, dim_z=1)
+        self.kf.F = np.array([[1., 1.], [0., 1.]])  # A
+        
+        self.kf.H = np.array([[1., 1.], [0., 1.]]) # C
+        self.kf.P *= 100.0  # Sigma_kp1
+        self.kf.R = np.array([[10., 0.], [0., 10.]]) # H
+        self.kf.Q = Q_discrete_white_noise(dim=2, dt=0.1, var=0.1)  # G
+        #self.human_direction = 0.0
+        self.objects=[]
+
+        self.tracking_first_valid_data = False
 
         
 
@@ -206,10 +234,133 @@ class LaserScanNode(Node):
         dynamic_data = [centr_list, bb_list]       
         
         return dynamic_data
+    def kalman_predict(self, state):
+            self.kf.x = np.array([[state[1]],[state[2]]])
+            self.kf.B = np.array([[state[3]],[0.]]) #B
+         # Predict object direction using Kalman filter
+            self.kf.predict()
+            #self.kf.update(np.array([[state[1]],[state[2]]]))
+            pos_x = self.kf.x[0]
+            pos_y = self.kf.x[1]
+            print("predicted =", pos_x, ",",pos_y)
+            #self.get_logger().info(f"Object Direction (Degrees): {object_direction_degrees}")
 
+    def call_kalman(self, centroids, header):
+         # Calculate cluster direction
+        cluster_data = np.array([[p[0],p[1]] for p in centroids])
+        new_obj_list=[]
+        #cluster_data = np.array([[point.x, point.y] for point in processed_msg.points])
+        print(len(cluster_data))
+        
+        if len(self.objects)==0:
+            it =1
+            for c in cluster_data:
+                obj_c = [it, c[0], c[1], 0, 1, 0] # obj_id, x,y, heading, lifetime, not seen
+                new_obj_list.append(obj_c)
+                #new_position = self.kalman_calculate(prev_state)
+                it+=1
+            
+
+        else:
+            
+            #new_obj_list =[]
+            visited_obs=[]
+
+            for prev_state in self.objects:
+                           
+                        min_dist = float('inf')
+                        for obs_id in range(len(cluster_data)):
+                            obs_x = cluster_data[obs_id][0]
+                            obs_y = cluster_data[obs_id][1]
+
+                            prev_state_id =      prev_state[0]
+                            prev_state_x =       prev_state[1]
+                            prev_state_y =       prev_state[2]
+                            prev_state_heading = prev_state[3]
+
+                            dist = np.sqrt((obs_x - prev_state_x)**2 + (obs_y - prev_state_y)**2)
+                            if dist < min_dist:
+                                min_dist = dist
+                                min_obj = prev_state
+                                obs_id_min_dist = obs_id
+
+                        visited_obs.append(obs_id_min_dist)
+                        min_obj[1] = obs_x
+                        min_obj[2] = obs_y
+                        min_obj[3] = np.arctan2((obs_y - prev_state_y)/(obs_x - prev_state_x))
+                        min_obj[4] +=1
+                        new_obj_list.append(min_obj)
+                
+                        
+            
+            for prev_state in self.objects:
+                if prev_state[0] not in [obj[0] for obj in new_obj_list] :
+                    prev_state[4] +=1
+                    prev_state[5] +=1
+                    new_position = self.kalman_predict(prev_state)
+                    prev_state[1] = new_position[0]
+                    prev_state[2] = new_position[1]
+                    new_obj_list.append(prev_state)
+
+            for i in range(len(cluster_data)):
+                    if i not in visited_obs:
+                        new_obj = [len(new_obj_list), cluster_data[i][0], cluster_data[i][1], 0, 1, 0]
+                        new_obj_list.append(new_obj)
+            
+            self.objects.clear()
+            self.objects = new_obj_list
+
+        data_cloud=[]
+        for obj in self.objects:
+            point32 = Point32()
+            point32.x = obj[1]
+            point32.y = obj[2]
+            point32.z = 0.0
+            data_cloud.append(point32)
+            point_cloud_msg_ = PointCloud()
+            point_cloud_msg_.header = header
+            point_cloud_msg_.points = data_cloud
+            self.human_direction_publisher.publish(point_cloud_msg_)
+
+
+
+
+
+
+
+            
+        #for i in range(len(cluster_data)):
+        #    cluster_direction_radians = np.arctan2(cluster_data[i][1], cluster_data[i][0])
+        #    cluster_direction_degrees = np.degrees(cluster_direction_radians)
+        #    self.get_logger().info(f"Cluster Direction (Degrees): {cluster_direction_degrees}")
+#
+        ## Predict object direction using Kalman filter
+        #    self.kf.predict()
+        #    self.kf.update(cluster_direction_degrees)
+        #    object_direction_degrees = self.kf.x[0]
+        #    self.get_logger().info(f"Object Direction (Degrees): {object_direction_degrees}")
+#
+        ## # Publish object direction
+        ## direction_msg = Int64()
+        ## direction_msg.data = int(object_direction_degrees)
+#
+        #    print(abs(object_direction_degrees - self.human_direction))
+        ## Check if the cluster direction is within the threshold to be considered a human
+        #
+        #    if abs(object_direction_degrees - self.human_direction) < HUMAN_DIRECTION_THRESHOLD:
+        #        self.get_logger().info(f"Human detected._{cluster_data[i]}")
+        #        point32 = Point32()
+        #        point32.x = cluster_data[i][0]
+        #        point32.y = cluster_data[i][1]
+        #        point32.z = 0.0
+        #        data_cloud.append(point32)
+        #        point_cloud_msg_ = PointCloud()
+        #        point_cloud_msg_.header = header
+        #        point_cloud_msg_.points = data_cloud
+        #        self.human_direction_publisher.publish(point_cloud_msg_)
 
     def process_data(self, point_cloud_msg):
-        global FRAME
+        #global FRAME
         data=[]
         for i in range(len(point_cloud_msg.points)):
             data.append([point_cloud_msg.points[i].x, point_cloud_msg.points[i].y, point_cloud_msg.points[i].z])
@@ -370,6 +521,8 @@ class LaserScanNode(Node):
         #print("centroids=", centroids)
 
         #increment FRAME and publish to /frameid topic
+
+        self.call_kalman(centroids, point_cloud_msg.header)
 
 
 
